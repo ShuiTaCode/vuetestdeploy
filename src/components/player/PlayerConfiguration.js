06a22eae5f090461b3fa50356ab862c7296e0778
Mario <mario.quitt@gmail.com>
2024-01-16 23:12:05 +0100

    new iteration

--- src/components/player/PlayerConfiguration.js
+++ src/components/player/PlayerConfiguration.js
@@ -5,7 +5,7 @@ const instruments ={}
 
 instruments[ApplicationConfig.drums.HIHAT] = {audio: new Audio("sounds/short/hihat.mp3"), path: "sounds/short/hihat.mp3",volume:0.1};
 instruments[ApplicationConfig.drums.SNDRM] = {audio: new Audio("sounds/snare.mp3"), path: "sounds/snare.mp3",volume:0.2};
-instruments[ApplicationConfig.drums.BSDRM] = {audio: new Audio("sounds/bassdrum.mp3"), path: "sounds/bassdrum.mp3",volume:1};
+instruments[ApplicationConfig.drums.BSDRM] = {audio: new Audio("sounds/bassdrum.mp3"), path: "sounds/bassdrum.mp3",volume:0.7};
 instruments[ApplicationConfig.drums.HITOM] = {audio: new Audio("sounds/hitom.mp3"), path: "sounds/hitom.mp3",volume:0.1};
 instruments[ApplicationConfig.drums.MDTOM] = {audio: new Audio("sounds/mdtom.mp3"), path: "sounds/mdtom.mp3",volume:0.1};
 instruments[ApplicationConfig.drums.LWTOM] = {audio: new Audio("sounds/lwtom.mp3"), path: "sounds/lwtom.mp3",volume:0.1};
