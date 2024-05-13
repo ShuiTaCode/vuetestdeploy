21b0a3cd8e74c142a54fe264333aa9ba8982365c
Mario <mario.quitt@gmail.com>
2024-05-13 20:27:20 +0200

    new iteration

--- src/clients/axoisClient.js
+++ src/clients/axoisClient.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
+// You can uncomment and use any of these API URLs as needed
 // const API_URL = '/restapi/v1/';
-// const API_URL = " http://192.168.178.41:3000/drumtracker";
-const API_URL = " https://shui-ta-express.ew.r.appspot.com/drumtracker";
+// const API_URL = "http://192.168.178.41:3000/drumtracker";
+const API_URL = "https://shui-ta-express.ew.r.appspot.com/drumtracker";
 // const API_URL = "https://wgtoffel-backend.ew.r.appspot.com/";
 // const API_URL =
 //   "https://3000-cs-1016443639495-default.cs-europe-west1-onse.cloudshell.dev/";
